93b6327dad88911f17ac78356d58ea839a5f9cab
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-02-26 00:04:16 +0600

    save

---
+++ examples/iceberg/main.py
@@ -0,0 +1,17 @@
+import asyncio
+import cherry_indexer as c
+
+
+asyncio
+
+async def main():
+    c.
+    config = c.Config(
+        project_name="iceberg_example",
+        description="Iceberg example",
+        pipelines={
+            "my_pipeline": c.
+        }
+    ) 
+
+asyncio.run(main())
