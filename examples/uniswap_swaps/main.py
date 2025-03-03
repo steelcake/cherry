8d4aec69b055841b9a1f82a22d7a6b6f0255ac59
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-03-03 20:47:26 +0600

    add swaps example

---
+++ examples/uniswap_swaps/main.py
@@ -0,0 +1,193 @@
+from clickhouse_connect.driver.asyncclient import AsyncClient
+import pyarrow as pa
+from cherry import config as cc
+from cherry.config import (
+    ClickHouseSkipIndex,
+    StepKind,
+    EvmDecodeEventsConfig,
+    # HexEncodeConfig,
+    CastConfig,
+)
+from cherry import run_pipelines, Context
+from cherry_core import ingest
+import logging
+import os
+import asyncio
+import clickhouse_connect
+from dotenv import load_dotenv
+import traceback
+from typing import Dict
+import argparse
+
+load_dotenv()
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "DEBUG").upper())
+logger = logging.getLogger(__name__)
+
+
+async def get_start_block(client: AsyncClient) -> int:
+    try:
+        res = await client.query("SELECT MAX(block_number) FROM swaps")
+        return res.result_rows[0][0] or 0
+    except Exception:
+        logger.warning(f"failed to get start block from db: {traceback.format_exc()}")
+        return 0
+
+
+def record_batch_from_schema(schema: pa.Schema) -> pa.RecordBatch:
+    arrays = []
+
+    for t in schema.types:
+        arrays.append(pa.array([], type=t))
+
+    return pa.record_batch(
+        arrays,
+        schema=schema,
+    )
+
+
+async def join_data(
+    data: Dict[str, pa.RecordBatch], _: cc.Step
+) -> Dict[str, pa.RecordBatch]:
+    blocks = pa.Table.from_batches([data["blocks"]])
+    swaps = pa.Table.from_batches([data["swaps"]])
+
+    blocks = blocks.rename_columns(["block_number", "block_timestamp"])
+
+    out = swaps.join(blocks, keys="block_number")
+
+    batches = out.to_batches()
+
+    out = (
+        pa.concat_batches(batches)
+        if len(batches) > 0
+        else record_batch_from_schema(out.schema)
+    )
+
+    return {"swaps": out}
+
+
+async def main(provider_kind: ingest.ProviderKind):
+    clickhouse_client = await clickhouse_connect.get_async_client(
+        host=os.environ.get("CLICKHOUSE_HOST", "localhost"),
+        port=int(os.environ.get("CLICKHOUSE_PORT", "8123")),
+        username=os.environ.get("CLICKHOUSE_USER", "default"),
+        password=os.environ.get("CLICKHOUSE_PASSWORD", "clickhouse"),
+        database=os.environ.get("CLICKHOUSE_DATABASE", "blockchain"),
+    )
+
+    from_block = await get_start_block(clickhouse_client)
+    logger.info(f"starting to ingest from block {from_block}")
+
+    event_signature = "Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to)"
+
+    provider = cc.Provider(
+        name="my_provider",
+        config=ingest.ProviderConfig(
+            kind=provider_kind,
+            url="https://portal.sqd.dev/datasets/ethereum-mainnet"
+            if provider_kind == ingest.ProviderKind.SQD
+            else None,
+            query=ingest.Query(
+                kind=ingest.QueryKind.EVM,
+                params=ingest.evm.Query(
+                    from_block=from_block,
+                    logs=[
+                        ingest.evm.LogRequest(
+                            event_signatures=[event_signature],
+                        )
+                    ],
+                    fields=ingest.evm.Fields(
+                        block=ingest.evm.BlockFields(number=True, timestamp=True),
+                        log=ingest.evm.LogFields(
+                            block_number=True,
+                            log_index=True,
+                            address=True,
+                            topic0=True,
+                            topic1=True,
+                            topic2=True,
+                            topic3=True,
+                            data=True,
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    )
+
+    writer = cc.Writer(
+        kind=cc.WriterKind.CLICKHOUSE,
+        config=cc.ClickHouseWriterConfig(
+            client=clickhouse_client,
+            order_by={"swaps": ["block_number"]},
+            skip_index={
+                "swaps": [
+                    ClickHouseSkipIndex(
+                        name="log_addr_idx",
+                        val="address",
+                        type_="bloom_filter(0.01)",
+                        granularity=1,
+                    ),
+                ]
+            },
+        ),
+    )
+
+    config = cc.Config(
+        project_name="my_project",
+        description="My description",
+        pipelines={
+            "my_pipeline": cc.Pipeline(
+                provider=provider,
+                writer=writer,
+                steps=[
+                    cc.Step(
+                        name="decode_swaps",
+                        kind=StepKind.EVM_DECODE_EVENTS,
+                        config=EvmDecodeEventsConfig(
+                            event_signature=event_signature,
+                            output_table="swaps",
+                        ),
+                    ),
+                    cc.Step(
+                        name="join_data",
+                        kind="join_data",
+                    ),
+                    cc.Step(
+                        name="cast_timestamp",
+                        kind=StepKind.CAST,
+                        config=CastConfig(
+                            table_name="swaps",
+                            mappings=[("block_timestamp", "Int64")],
+                        ),
+                    ),
+                    # cc.Step(
+                    #     name="prefix_hex_encode",
+                    #     kind=StepKind.HEX_ENCODE,
+                    #     config=HexEncodeConfig(),
+                    # ),
+                ],
+            )
+        },
+    )
+
+    context = Context()
+
+    context.add_step("join_data", join_data)
+
+    await run_pipelines(config, context)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Clickhouse example")
+
+    parser.add_argument(
+        "--provider",
+        choices=["sqd", "hypersync"],
+        required=True,
+        help="Specify the provider ('sqd' or 'hypersync')",
+    )
+
+    args = parser.parse_args()
+
+    asyncio.run(main(args.provider))
