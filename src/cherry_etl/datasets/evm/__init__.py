74f50e6d54a2089176d8fdbad47ef74631f8585c
Metin Demir <metmirr@gmail.com>
2025-03-25 22:33:05 +0200

    feat(dataset): add blocks dataset (#42)

--- src/cherry_etl/datasets/evm/__init__.py
+++ src/cherry_etl/datasets/evm/__init__.py
@@ -1,3 +1,4 @@
 from .address_appearances import make_pipeline as address_appearances
+from .blocks import make_pipeline as blocks
 
-__all__ = ["address_appearances"]
+__all__ = ["address_appearances", "blocks"]
