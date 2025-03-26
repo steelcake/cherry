de8ca30c1b82004e9dacc320fad4a3b0203f6876
Metin Demir <metmirr@gmail.com>
2025-03-26 21:38:25 +0200

    feat(datasets): add all_contracts dataset

--- src/cherry_etl/datasets/evm/__init__.py
+++ src/cherry_etl/datasets/evm/__init__.py
@@ -1,4 +1,5 @@
 from .address_appearances import make_pipeline as address_appearances
+from .all_contracts import make_pipeline as all_contracts
 from .blocks import make_pipeline as blocks
 
-__all__ = ["address_appearances", "blocks"]
+__all__ = ["address_appearances", "all_contracts", "blocks"]
