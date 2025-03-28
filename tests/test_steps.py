dde9c26178920e8ac55632f23fa390e86295aeb0
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-03-28 14:00:09 +0600

    save

---
+++ tests/test_steps.py
@@ -0,0 +1,20 @@
+from cherry_etl import steps as cs
+from cherry_etl import config as cc
+
+def test_base58_encode():
+    return
+
+def test_cast():
+    return
+
+def test_cast_by_type():
+    return
+
+def test_evm_decode_events():
+    return
+
+def test_evm_validate_block_data():
+    return
+
+def test_hex_encode():
+    return
